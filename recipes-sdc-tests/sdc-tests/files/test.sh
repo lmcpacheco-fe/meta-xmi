cf2facbab3bbea3f4b33b51e743db72e54a382a1
Aitor Carrizosa <aitor.carrizosa@futureelectronics.com>
2026-04-10 11:02:55 -0400

    Add bring-up changes for rev1 on meta-xmi

--- recipes-sdc-tests/sdc-tests/files/test.sh
+++ recipes-sdc-tests/sdc-tests/files/test.sh
@@ -14,7 +14,6 @@ function evaluatetest {
 	fi
 }
 
-
 echo "#######################################"
 echo "# Testing Analog to Digical Converter #"
 echo "#######################################"
@@ -24,53 +23,48 @@ ADCDIR=$(find /sys/devices/platform -name iio:device*)
 cat $ADCDIR/in_voltage*_raw
 evaluatetest "Were the numbers printed correct?"
 
+echo "########################"
+echo "# Testing DDR Memory   #"
+echo "########################"
+echo "Running memtester..."
+memtester 128M 1
+evaluatetest "Did the DDR memtester pass?"
 
+# echo "######################################"
+# echo "# Testing Microphone - Say Something #"
+# echo "#      Press Enter To Continue       #"
+# echo "######################################"
+# read
+# # this shouldn't be bodged in test, but properly fixed
+# # mv /etc/asound.conf /etc/asound.conf.bak
+# # should sometimes be hw:1,0
+# # I'm not sure how to fix that
+# arecord -D hw:2,0 -f S32_LE -d 5 out.wav
 
-echo "######################################"
-echo "# Testing Microphone - Say Something #"
-echo "#      Press Enter To Continue       #"
-echo "######################################"
-read
+# echo "###################################"
+# echo "# Playing Back the Recorded Audio #"
+# echo "###################################"
+# aplay out.wav
+# evaluatetest "Did you hear the recorded audio playing back?"
 
-# this shouldn't be bodged in test, but properly fixed
-# mv /etc/asound.conf /etc/asound.conf.bak
-
-# should sometimes be hw:1,0
-# I'm not sure how to fix that
-arecord -D hw:2,0 -f S32_LE -d 5 out.wav
-
-echo "###################################"
-echo "# Playing Back the Recorded Audio #"
-echo "###################################"
-aplay out.wav
-evaluatetest "Did you hear the recorded audio playing back?"
-
-
-
-echo "##########################################"
-echo "# Testing Speaker - Playing Sample Audio #"
-echo "#        Press Enter To Continue         #"
-echo "##########################################"
-read
-aplay -d 4 /etc/sdc/Moldova.wav
-evaluatetest "Did the Speaker test pass?"
-
-
-
-# read -p "Please enter the WiFi SSID: " SSID
-read -p "please enter the WiFi Password for FE-IoT: " PASSWORD
-
-SSID="wifi_24cd8d911496_46452d494f54_managed_psk"
+# echo "##########################################"
+# echo "# Testing Speaker - Playing Sample Audio #"
+# echo "#        Press Enter To Continue         #"
+# echo "##########################################"
+# read
+# aplay -d 4 /etc/sdc/Moldova.wav
+# evaluatetest "Did the Speaker test pass?"
+# # read -p "Please enter the WiFi SSID: " SSID
+# read -p "please enter the WiFi Password for FE-IoT: " PASSWORD
+# SSID="wifi_24cd8d911496_46452d494f54_managed_psk"
 
 echo "########################################"
 echo "# Testing WiFi - Connecting to Network #"
-echo "# SSID=$SSID Password=$PASSWORD    #"
+echo "# SSID=$SSID Password=$PASSWORD    	 #"
 echo "#       Press Enter To Continue        #"
 echo "########################################"
 read
-
 modprobe moal mod_para=nxp/wifi_mod_para.conf
-
 expect -c '
 	spawn connmanctl
 	expect "connmanctl> "
@@ -86,15 +80,11 @@ expect -c '
 	send "quit\r"
 '
 echo $PASSWORD | connmanctl connect $SSID
-
 evaluatetest "Did the WiFi test pass?"
-
-
 
 echo "#####################"
 echo "# Testing Bluetooth #"
 echo "#####################"
-
 systemctl start bluetooth.service
 modprobe btnxpuart
 hciconfig hci0 up
@@ -110,34 +100,29 @@ expect -c '
 echo
 evaluatetest "Did the Bluetooth test pass?"
 
-
-
-echo "################"
-echo "# Testing LEDs #"
-echo "################"
-
-echo 255 > /sys/class/leds/pca963x\:red/brightness
-echo 255 > /sys/class/leds/pca963x\:green/brightness
-echo 255 > /sys/class/leds/pca963x\:blue/brightness
-sleep 1
-echo 0 > /sys/class/leds/pca963x\:red/brightness
-sleep 1
-echo 255 > /sys/class/leds/pca963x\:red/brightness
-echo 0 > /sys/class/leds/pca963x\:green/brightness
-sleep 1
-echo 0 > /sys/class/leds/pca963x\:red/brightness
-sleep 1
-echo 255 > /sys/class/leds/pca963x\:red/brightness
-echo 255 > /sys/class/leds/pca963x\:green/brightness
-echo 0 > /sys/class/leds/pca963x\:blue/brightness
-sleep 1
-echo 0 > /sys/class/leds/pca963x\:red/brightness
-sleep 1
-echo 255 > /sys/class/leds/pca963x\:red/brightness
-echo 0 > /sys/class/leds/pca963x\:green/brightness
-sleep 1
-echo 0 > /sys/class/leds/pca963x\:red/brightness
-sleep 1
-
-evaluatetest "Did the LED test pass?"
-
+# echo "################"
+# echo "# Testing LEDs #"
+# echo "################"
+# echo 255 > /sys/class/leds/pca963x\:red/brightness
+# echo 255 > /sys/class/leds/pca963x\:green/brightness
+# echo 255 > /sys/class/leds/pca963x\:blue/brightness
+# sleep 1
+# echo 0 > /sys/class/leds/pca963x\:red/brightness
+# sleep 1
+# echo 255 > /sys/class/leds/pca963x\:red/brightness
+# echo 0 > /sys/class/leds/pca963x\:green/brightness
+# sleep 1
+# echo 0 > /sys/class/leds/pca963x\:red/brightness
+# sleep 1
+# echo 255 > /sys/class/leds/pca963x\:red/brightness
+# echo 255 > /sys/class/leds/pca963x\:green/brightness
+# echo 0 > /sys/class/leds/pca963x\:blue/brightness
+# sleep 1
+# echo 0 > /sys/class/leds/pca963x\:red/brightness
+# sleep 1
+# echo 255 > /sys/class/leds/pca963x\:red/brightness
+# echo 0 > /sys/class/leds/pca963x\:green/brightness
+# sleep 1
+# echo 0 > /sys/class/leds/pca963x\:red/brightness
+# sleep 1
+# evaluatetest "Did the LED test pass?"
