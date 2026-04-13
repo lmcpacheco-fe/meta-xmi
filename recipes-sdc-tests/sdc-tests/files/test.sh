150eecae3690ce3445d8ae7784a80696518f94fd
Aitor Carrizosa <aitor.carrizosa@futureelectronics.com>
2026-04-13 15:19:07 -0400

    Modify test script for rev1

--- recipes-sdc-tests/sdc-tests/files/test.sh
+++ recipes-sdc-tests/sdc-tests/files/test.sh
@@ -15,13 +15,25 @@ function evaluatetest {
 }
 
 echo "#######################################"
-echo "# Testing Analog to Digical Converter #"
+echo "# Checking kernel logs for errors     #"
 echo "#######################################"
-ADCDIR=$(find /sys/devices/platform -name iio:device*)
-# usually /sys/devices/platform/soc@0/30800000.bus/30800000.spba-bus/30820000.spi/spi_master/spi1/spi1.1/iio:device0
-# but sometimes slightly different
-cat $ADCDIR/in_voltage*_raw
-evaluatetest "Were the numbers printed correct?"
+ERRORS=$(dmesg 2>/dev/null | grep -i "error")
+if [ -n "$ERRORS" ]; then
+    echo "Kernel errors detected:"
+    echo "$ERRORS"
+else
+    echo "No kernel errors found in kernel logs"
+fi
+evaluatetest "Did the kernel log test pass?"
+
+# echo "#######################################"
+# echo "# Testing Analog to Digital Converter #"
+# echo "#######################################"
+# ADCDIR=$(find /sys/devices/platform -name iio:device*)
+# # usually /sys/devices/platform/soc@0/30800000.bus/30800000.spba-bus/30820000.spi/spi_master/spi1/spi1.1/iio:device0
+# # but sometimes slightly different
+# cat $ADCDIR/in_voltage*_raw
+# evaluatetest "Were the numbers printed correct?"
 
 echo "########################"
 echo "# Testing DDR Memory   #"
@@ -54,32 +66,51 @@ evaluatetest "Did the DDR memtester pass?"
 # read
 # aplay -d 4 /etc/sdc/Moldova.wav
 # evaluatetest "Did the Speaker test pass?"
-# # read -p "Please enter the WiFi SSID: " SSID
-# read -p "please enter the WiFi Password for FE-IoT: " PASSWORD
-# SSID="wifi_24cd8d911496_46452d494f54_managed_psk"
 
 echo "########################################"
 echo "# Testing WiFi - Connecting to Network #"
-echo "# SSID=$SSID Password=$PASSWORD    	 #"
-echo "#       Press Enter To Continue        #"
 echo "########################################"
+read -p "Please enter the WiFi SSID: " WIFI_NAME
+read -s -p "Please enter the WiFi password: " PASSWORD
+echo
+echo "#       Press Enter To Continue        #"
 read
 modprobe moal mod_para=nxp/wifi_mod_para.conf
-expect -c '
-	spawn connmanctl
-	expect "connmanctl> "
-	send "agent on\r"
-	expect "connmanctl> "
-	send "enable wifi\r"
-	expect "connmanctl> "
-	send "services\r"
-	expect "connmanctl> "
-	send "connect '$SSID'\r"
-	expect "Passphrase? "
-	send "'$PASSWORD'\r"
-	send "quit\r"
-'
-echo $PASSWORD | connmanctl connect $SSID
+connmanctl enable wifi >/dev/null 2>&1
+connmanctl scan wifi >/dev/null 2>&1
+echo "Available WiFi services:"
+connmanctl services
+
+SERVICE=$(connmanctl services | awk -v ssid="$WIFI_NAME" '$1 == ssid {print $NF; exit}')
+
+if [ -z "$SERVICE" ]; then
+    echo "WiFi service for SSID '$WIFI_NAME' not found"
+else
+    echo "Connecting to $WIFI_NAME ..."
+    expect <<EOF
+spawn connmanctl
+expect "connmanctl> "
+send "agent on\r"
+expect "connmanctl> "
+send "connect $SERVICE\r"
+expect {
+    "Passphrase? " {
+        send "$PASSWORD\r"
+        expect "connmanctl> "
+    }
+    "connmanctl> " {
+    }
+}
+send "quit\r"
+expect eof
+EOF
+    echo
+    echo "########################"
+    echo "# Pinging google.com   #"
+    echo "########################"
+    sleep 5
+    ping -c 4 google.com
+fi
 evaluatetest "Did the WiFi test pass?"
 
 echo "#####################"
@@ -94,8 +125,11 @@ expect -c '
 	send "agent on\r"
 	expect "\[bluetooth\]# "
 	send "scan on\r"
+	sleep 10
+	send "scan off\r"
 	expect "\[bluetooth\]# "
-	send "quit"
+	send "quit\r"
+	expect eof
 '
 echo
 evaluatetest "Did the Bluetooth test pass?"
@@ -126,3 +160,33 @@ evaluatetest "Did the Bluetooth test pass?"
 # echo 0 > /sys/class/leds/pca963x\:red/brightness
 # sleep 1
 # evaluatetest "Did the LED test pass?"
+
+echo "#######################"
+echo "# Testing Temperature #"
+echo "#######################"
+if [ -d /sys/class/thermal ]; then
+    for zone in /sys/class/thermal/thermal_zone*; do
+        [ -f "$zone/temp" ] || continue
+
+        NAME=$(cat "$zone/type" 2>/dev/null)
+        TEMP=$(cat "$zone/temp" 2>/dev/null)
+
+        case "$TEMP" in
+            ''|*[!0-9]*)
+                echo "$NAME: N/A"
+                continue
+                ;;
+        esac
+
+        if [ "$TEMP" -ge 1000 ]; then
+            INT=$(( TEMP / 1000 ))
+            DEC=$(( (TEMP % 1000) / 100 ))
+            printf "%s: %d.%01d °C\n" "$NAME" "$INT" "$DEC"
+        else
+            printf "%s: %d °C\n" "$NAME" "$TEMP"
+        fi
+    done
+else
+    echo "No thermal zones found"
+fi
+evaluatetest "Did the temperature test pass?"
